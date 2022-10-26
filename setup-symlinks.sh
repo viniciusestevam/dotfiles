76dfee8f2ad58e158295f0af61098a9bc992a3f0
Vinicius Estevam <estevamvinicius31@gmail.com>
2022-10-25 21:14:31 -0300

    add setup-symlinks.sh

---
+++ setup-symlinks.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/allacrity.yml ~/.config/allacrity/allacrity.yml
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.xmonad ~/.xmonad
